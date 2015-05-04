0e5bde6250ede41000c0771ac36dc98b260c6d3a
Breno Thales <breno.thales@gmail.com>
2015-05-04 12:33:12 -0500

    recurso de layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -15,4 +15,6 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/avatar.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
 end
