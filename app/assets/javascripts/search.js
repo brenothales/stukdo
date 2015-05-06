9729b5e755f7ec9f63c2d4c4f38585d7a4bafb6f
Breno Thales <breno.thales@gmail.com>
2015-05-06 13:50:58 -0500

    recurso de pesquisar em todas as views

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -44,3 +44,13 @@ $(document).ready(function() {
         }
     });
 });
+
+$(function() {    
+    $('#system-search').on('keyup', function() {
+      var rex = new RegExp($(this).val(), 'i');
+        $('.searchable-container .items').hide();
+        $('.searchable-container .items').filter(function() {
+            return rex.test($(this).text());
+        }).show();
+    });
+});
